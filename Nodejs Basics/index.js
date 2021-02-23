0e0d899433f898a38852fd7cafe9bfc6db2a9281
RichaSundrani <sundrani1177@gmail.com>
2021-02-23 11:22:51 -0600

    First commit

---
+++ Nodejs Basics/index.js
@@ -0,0 +1,17 @@
+// const Person = require('./person');
+
+// let person1 = new Person('Richard Parkar', 32);
+// let person2 = new Person('Nimmu', 30);
+// let person3 = new Person('Kdg', 51);
+// person1.greeting();
+// person2.greeting();
+// person3.greeting();
+
+
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((resquest, response) => {
+    console.log()
+});
