12966a41d7dcdbdc7e07bebbfdeac8f95d3eece6
Himalayan Rain <57468079+RichaSundrani@users.noreply.github.com>
2021-02-23 13:36:00 -0600

    Delete Nodejs Basics directory

--- Nodejs Basics/http.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-//Create server object 
-http.createServer((request, response)=>{
-    // Write response
-    response.write('Hello Richard Parkar'); 
-    response.end();
-}).listen(5000, ()=> console.log('Server running ... '));
